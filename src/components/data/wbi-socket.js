fdadbe4ff7ba9c9b448e4c92f27ad30b8e11ed53
bkawk <email.will.in.china@gmail.com>
2019-04-17 20:10:52 +0800

    connect

--- src/components/data/wbi-socket.js
+++ src/components/data/wbi-socket.js
@@ -8,7 +8,6 @@ class WbiSocket extends ReduxMixin(PolymerElement) {
   static get template() {
     return html`
       <app-location route="{{route}}" url-space-regex="^[[rootPath]]"></app-location>
-      <script src="../../../socket.io.js"></script>
     `;
   }
 
@@ -29,10 +28,20 @@ class WbiSocket extends ReduxMixin(PolymerElement) {
       env: state.env,
     };
   }
+  ready() {
+    super.ready();
+    console.log('ready');
+    this._connect();
+  }
   _connect() {
+    console.log('_connect called');
     this.jwt = localStorage.getItem('jwt');
-    this.socket = io(this.env.socketUrl, {query: `jwt=${this.jwt}`});
+    console.log('this.jwt');
+    console.log(this.jwt);
+    this.socket = io('https://dev-api.worbli.io/ws/v1/', {query: `jwt=${this.jwt}`});
+    console.log(this.socket);
     this.socket.on('connect', () => {
+      console.log('connected');
       this.socket.on('status', (data) => {
         console.log(data);
       });
