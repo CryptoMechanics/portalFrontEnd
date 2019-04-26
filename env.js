93c5d252511f7bfb829819a42fee9147b5f4ad05
Will Hill <bkawk@users.noreply.github.com>
2019-04-26 09:34:51 +0800

    Update env.js
    env update for socket param

--- env.js
+++ env.js
@@ -2,6 +2,7 @@ export const env = {
   siteName: 'Worbli',
   webAddress: 'www.worbli.io',
   apiUrl: 'https://uat-api.worbli.io/api/v3',
+  socketUrl: 'https://uat-api.worbli.io/',
   googleTagManager: 'GTM-PM5T86M',
   social: {
     facebook: 'test.com',
