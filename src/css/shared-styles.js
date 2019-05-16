f9702bedf74ec16ca6a95d5381d887a274c31f54
bkawk <email.will.in.china@gmail.com>
2019-05-16 18:18:04 +0800

    card contrast

--- src/css/shared-styles.js
+++ src/css/shared-styles.js
@@ -11,7 +11,7 @@ $_documentContainer.innerHTML = `<dom-module id="shared-styles">
       .card {
         margin: 24px;
         padding: 48px;
-        color: #50595E;
+        color: #323538;
         border-radius: 5px;
         background-color: #fff;
         box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
