5c6a4043f98d6fec5cbcbdec9e1311624459f4a1
bkawk <email.will.in.china@gmail.com>
2019-05-28 23:22:25 -0600

    connect env

--- env.js
+++ env.js
@@ -3,7 +3,23 @@
 export const env = {
   siteName: 'Worbli',
   webAddress: 'www.worbli.io',
-  googleTagManager: 'GTM-PM5T86M',
+  connect: {
+    dev: {
+      domainPrefix: 'dev',
+      apiUrl: 'https://dev-api.worbli.io/api/v3',
+      socketUrl: 'https://dev-api.worbli.io/',
+    },
+    uat: {
+      domainPrefix: 'uat',
+      apiUrl: 'https://uat-api.worbli.io/api/v3',
+      socketUrl: 'https://uat-api.worbli.io/',
+    },
+    www: {
+      domainPrefix: 'www',
+      apiUrl: 'https://api-api.worbli.io/api/v3',
+      socketUrl: 'https://api-api.worbli.io/',
+    },
+  },
   social: {
     facebook: 'https://www.facebook.com/worbli',
     twitter: 'https://twitter.com/worbli',
